8683cf1a655456f8c7eae481a3698c07a4526e6d
gokcam <gokhancam55@gmail.com>
2021-01-22 21:45:36 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<Word ,Mean>
+    {
+        Word[] _words;
+        Mean[] _means;
+
+        Word[] _tempArrayWord;
+        Mean[] _tempArrayMeans;
+
+        public MyDictionary()
+        {
+            _words = new Word[0];
+            _means = new Mean[0]; 
+        }
+
+        public void Add(Word item, Mean item2)
+        {
+            _tempArrayWord = _words;
+            _tempArrayMeans = _means;
+
+            _words = new Word[_words.Length + 1];
+            _means = new Mean[_means.Length + 1];
+            for (int i = 0; i < _tempArrayWord.Length; i++)
+            {
+                _tempArrayWord[i] = _words[i];
+            }
+            for (int i = 0; i < _tempArrayMeans.Length; i++)
+            {
+                _tempArrayMeans[i] = _means[i];
+            }
+
+            _words[_words.Length - 1] = item;
+            _means[_means.Length - 1] = item2;
+        }
+        public Word[] Keys
+        {
+            get { return _words; }
+        }
+
+        public Mean[] Keys2
+        {
+            get { return _means; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cities = new MyDictionary<int,string>();
+            cities.Add(55, "Samsun");
+            cities.Add(07, "Antalya");
+            cities.Add(61, "Trabzon");
+            cities.Add(53, "Rize");
+            cities.Add(28, "Giresun");
+
+            foreach (var city in cities.Keys)
+            {
+                Console.WriteLine(city);
+            }
+
+            foreach(var city2 in cities.Keys2)
+            {
+                Console.WriteLine(city2);
+            }
+
+        }
+        
+    }
+}
